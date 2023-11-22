a460cad24339926f32aa9e32489fdf1b98cd6d36
Rafaelgeomo <65200578+Rafaelgeomo@users.noreply.github.com>
2023-11-22 19:00:30 -0300

    captura de imagem

--- js/camera.js
+++ js/camera.js
@@ -1,6 +1,11 @@
 const botaoInciarCamera = document.querySelector("[data-video-botao]");
 const campoCamera = document.querySelector("[data-camera]");
 const video = document.querySelector("[data-video]");
+const botaoTirarFoto = document.querySelector("[data-tirar-foto]");
+const canvas = document.querySelector("[data-video-canvas]");
+const mensagem = document.querySelector("[data-mensagem]");
+
+let imagemURL = "";
 
 botaoInciarCamera.addEventListener("click", async function (){
     const iniciarVideo = await navigator.mediaDevices.getUserMedia({video:true, audio:false})
@@ -8,7 +13,16 @@ botaoInciarCamera.addEventListener("click", async function (){
     botaoInciarCamera.style.display = "none";
     campoCamera.style.display = "block";
 
-    video.srcObject = iniciarVideo;
+    video.srcObject = iniciarVideo;    
+})
 
-    
+botaoTirarFoto.addEventListener("click", function(){
+    canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
+
+    imagemURL = canvas.toDataURL("image/jpeg");
+
+    campoCamera.style.display = "none";
+    mensagem.style.display = "block";
+
+    video.srcObject.getTracks().forEach(track => track.stop())
 })
